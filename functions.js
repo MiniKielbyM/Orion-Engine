f632bd89bec989290e5b3636936d2bd6e546d0ac
MiniKielbyM <85803354+MiniKielbyM@users.noreply.github.com>
2023-01-28 13:06:08 +0000

    confirm placement

--- functions.js
+++ functions.js
@@ -1,35 +1,33 @@
-var elemSelectName = null;
+var elemSelectName = "blank";
+function confirmPlacement() {
+    elemSelectName = "blank";
+}
 function confirmPlayer() {
-    console.log("press confirmed")
-    // Get the element
-    var elem = document.querySelector('#playerObjBase');
+    if (elemSelectName == "blank") {
+        console.log("press confirmed")
+        // Get the element
+        var elem = document.querySelector('#playerObjBase');
 
-    // Create a copy of it
-    var clone = elem.cloneNode(false);
+        // Create a copy of it
+        var clone = elem.cloneNode(true);
 
-    // Update the ID and add a class
-    clone.id = 'playerObj';
+        // Update the ID and add a class
+        clone.id = 'playerObj';
 
-    // Inject it into the DOM
-    elem.before(clone);
-    elemSelectName = "playerObj"
+        // Inject it into the DOM
+        elem.before(clone);
+        elemSelectName = "playerObj"
+        console.log(elemSelectName)
+    }
 }
 onmousemove = function (e) {
-    if (!elemSelectName === null)
-    {
-        //Logging purposes
-        console.log("mouse location:", e.clientX, e.clientY);
+    //Logging purposes
+    console.log("mouse location:", e.clientX, e.clientY);
 
-        //meat and potatoes of the snippet
-        var pos = e;
-        var element;
-        element = document.getElementById(elemSelectName)
-        element.style.left = pos.x + "px";
-        element.style.top = pos.y + "px";
-    }
-}      
-
-function stopMove()
-{
-    elemSelectName = null;
+    //meat and potatoes of the snippet
+    var pos = e;
+    var element;
+    element = document.getElementById(elemSelectName)
+    element.style.left = pos.x + "px";
+    element.style.top = pos.y + "px";
 }
